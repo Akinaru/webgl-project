8aa863e26da3d4e27fff29215f53c5f3f516afe4
Maxime GALLOTTA <maxime@genesii.fr>
2026-04-27 16:29:52 +0200

    Dialogues sons

--- src/Experience/Source/sources.audio.js
+++ src/Experience/Source/sources.audio.js
@@ -8,5 +8,15 @@ export default [
         name: 'pauseOpenSound',
         type: 'audioBuffer',
         path: 'sounds/ui/pause-open.mp3'
+    },
+    {
+        name: 'voice_1',
+        type: 'audioBuffer',
+        path: 'sounds/dialogues/voice_1.mp3'
+    },
+    {
+        name: 'voice_2',
+        type: 'audioBuffer',
+        path: 'sounds/dialogues/voice_2.mp3'
     }
 ]
