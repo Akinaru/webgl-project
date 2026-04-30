ecad29486f66899d957a00569ed863e3280fb552
Thibaud <thibaud.evrard@cern.ch>
2026-04-30 11:35:25 +0200

    Fix warning sound

--- src/Experience/Audio/bushSoundBank.js
+++ src/Experience/Audio/bushSoundBank.js
@@ -1,25 +1,11 @@
-const bushSoundModules = {
-    ...import.meta.glob('/public/sounds/effects/bush/*.{mp3,ogg,wav,m4a,webm}', {
-        eager: true,
-        import: 'default'
-    }),
-    ...import.meta.glob('../../../../public/sounds/effects/bush/*.{mp3,ogg,wav,m4a,webm}', {
-        eager: true,
-        import: 'default'
-    })
-}
-
 const fallbackBushSoundUrls = [
     '/sounds/effects/bush/bush.mp3',
     '/sounds/effects/bush/bush-1.mp3'
 ]
 
-const globBushSoundUrls = Object.values(bushSoundModules)
-    .filter((value) => typeof value === 'string' && value.length > 0)
-
 export function getBushSoundUrls()
 {
-    return [...globBushSoundUrls, ...fallbackBushSoundUrls]
+    return [...fallbackBushSoundUrls]
         .filter((value, index, list) =>
             typeof value === 'string'
             && value.length > 0
